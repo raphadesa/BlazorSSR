6deef144954a5a649c08ebe68aea990ea6af93a7
Raphaël Desalbres <raphael.desalbres@messages.fr>
2025-07-20 13:20:20 +0200

    Ajoutez des fichiers projet.

---
+++ BlazorSSR/Contact.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorSSR
+{
+    public class Contact
+    {     
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [Range(typeof(bool), "true", "true", ErrorMessage = "The captcha is not valid!")]
+        public bool IsRecaptchaValid { get; set; }
+        
+    }
+}
